a4cc56d0cb4386b0c7ec26557108c32ce4af4233
TranHuy <tranhuy261292@gmail.com>
2024-01-10 21:51:06 +0700

    complete project

---
+++ Animation Clock/script.js
@@ -0,0 +1,16 @@
+function updateClock() {
+  const now = new Date();
+  const hours = now.getHours() % 12;
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  const hourDeg = hours * 30 + (minutes / 60) * 30;
+  const minuteDeg = minutes * 6 + (seconds / 60) * 6;
+  const secondDeg = seconds * 6;
+
+  document.getElementById("hour").style.transform = `rotate(${hourDeg}deg)`;
+  document.getElementById("minute").style.transform = `rotate(${minuteDeg}deg)`;
+  document.getElementById("second").style.transform = `rotate(${secondDeg}deg)`;
+}
+
+setInterval(updateClock, 1000);
